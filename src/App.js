3f14ca2ccfae80f867d66e6583fe3ffa396bfdb0
Chris Sanders <christopherdksanders@gmail.com>
2020-02-05 16:32:38 -0800

    adding header and links shop and contact in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import './App.css'
+
 import Homepage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/Shop'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         {/* if you are navigating from index page (/) no slashes needed in MenuItem else you do need them */}
         <Route exact path='/' component={Homepage} />
